d1a1c3bc90a0ab9db65bc5c7869c6ac45878672f
Georg Mueksch <georg.mueksch@jumio.com>
2015-01-15 15:13:08 +0100

    refactoring

--- src/main/groovy/com/android/camera2/CameraActivity.groovy
+++ src/main/groovy/com/android/camera2/CameraActivity.groovy
@@ -12,9 +12,13 @@ class CameraActivity extends AndroidBaseActivity{
         done(required:false,wait:true){ $("#com.android.camera2:id/done_button")}
 
         takePicture {
-            shutter.click()
-            if(!done.isDisplayed() ) back()
-            else done.click()
+            waitFor(10,1) {
+                shutter.click()
+                if (done.isDisplayed())
+                    done.click()
+                else
+                    back()
+            }
         }
 
 
