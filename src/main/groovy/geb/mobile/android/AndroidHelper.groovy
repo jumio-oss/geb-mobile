2e0c5610c2a26bb9b6c63695d364c7bca633ebef
Georg Mueksch <georg.mueksch@jumio.com>
2015-01-28 10:22:38 +0100

    Rename Presenter, add presenter call to remote when running on jenkins

--- src/main/groovy/geb/mobile/android/AndroidHelper.groovy
+++ src/main/groovy/geb/mobile/android/AndroidHelper.groovy
@@ -1,14 +1,23 @@
 package geb.mobile.android
 
+import geb.Browser
+import groovy.transform.Trait
 import io.appium.java_client.android.AndroidDriver
 import io.appium.java_client.android.AndroidKeyCode
 import io.selendroid.SelendroidKeys
+import org.openqa.selenium.WebDriver
 import org.openqa.selenium.interactions.Actions
 
 /**
  * Created by gmueksch on 21/01/15.
  */
 class AndroidHelper {
+
+    private Browser browser
+
+    private WebDriver getDriver(){
+        browser.driver
+    }
 
     void back() {
         if (driver instanceof AndroidDriver) driver.sendKeyEvent(AndroidKeyCode.BACK)
@@ -26,6 +35,23 @@ class AndroidHelper {
     }
 
     public String getMessage(){
-        $("#android:id/message").text()
+        browser.find("#android:id/message").text()
+    }
+
+    public def systemRetry(){
+        browser.find("#android:id/button1").click()
+    }
+
+    public def systemCancel(){
+        browser.find("#android:id/button2").click()
+    }
+
+    public void handleSystemMessage(){
+        def msg = getMessage()
+        if( msg == "We have encountered a network communication problem" ) {
+            systemRetry()
+        }
+        true
+
     }
 }
